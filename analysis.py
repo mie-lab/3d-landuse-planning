fa2efde18a418b8290b9d18679e704e83ce5357a
AydaGri <agrisiute@ethz.ch>
2025-01-07 16:03:46 +0100

    Minor plotting style changes. Added plot_scenario_difference ploting function.

--- analysis.py
+++ analysis.py
@@ -1,13 +1,19 @@
+import logging
 import os
 import contextily as ctx
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.colorbar import ColorbarBase
-from matplotlib.colors import BoundaryNorm, LinearSegmentedColormap
+import matplotlib.patches as mpatches
+from matplotlib.colors import BoundaryNorm, LinearSegmentedColormap, ListedColormap
+from matplotlib.lines import Line2D
+import geopandas as gpd
 
-from retrievers import get_gfas, get_regulation_counts, get_regulation_plot_counts,\
+from retrievers import get_gfas, get_regulation_counts, get_regulation_plot_counts, \
     get_frequent_regulation_instances, get_plot_ids
+
+logger = logging.getLogger(__name__)
 
 
 def get_regulation_overview_df(endpoint, reg_names, plots, out_dir=None):
@@ -46,6 +52,7 @@ def get_regulation_overview_df(endpoint, reg_names, plots, out_dir=None):
     if out_dir:
         output_file = os.path.join(out_dir, 'output_table_2.csv')
         df.to_csv(output_file, index=True)
+        logger.info(f"Data for Table 2 saved to {out_dir}")
 
     return df
 
@@ -60,12 +67,12 @@ def get_regulation_instance_overview_df(endpoint, plots, out_dir=None):
     :return: A DataFrame summarizing regulations and their respective plot areas.
     """
 
-    sampled_regs = get_frequent_regulation_instances(endpoint)
+    qr = get_frequent_regulation_instances(endpoint)
+    sampled_regs = qr.drop_duplicates(subset=['programmes', 'type', 'plots'])
     results = []
 
     for _, row in sampled_regs.iterrows():
         reg_id = row['reg']
-
         plot_ids = get_plot_ids(endpoint, reg_id)
         subset_plots = plots[plots['plots'].isin(plot_ids)]
         total_area = round(subset_plots['plot_area'].sum() / 1e6, 3)
@@ -75,15 +82,15 @@ def get_regulation_instance_overview_df(endpoint, plots, out_dir=None):
             'programmes': row['programmes'],
             'zones': row['zones'],
             'plots_count': len(plot_ids),
-            'total_area': total_area
+            'total_area_km': total_area
         })
 
-    df = pd.DataFrame(results)[:10]
+    df = pd.DataFrame(results)
 
     if out_dir:
         output_file = os.path.join(out_dir, 'output_table_3.csv')
         df.to_csv(output_file, index=True)
-        print(f"DataFrame written to {out_dir}")
+        logger.info(f"Data for Table 3 saved to {out_dir}")
 
     return df
 
@@ -97,10 +104,11 @@ def get_gfa_overview(endpoint, plots, non_gfa_plots, out_dir):
     :param plots: GeoDataFrame containing plot data with 'plots', 'zone', 'gpr', and 'plot_area' columns.
     :param non_gfa_plots: List of plot identifiers to exclude from the analysis.
     """
+
     # Calculate the metrics
     plot_counts = plots.groupby('zone')['plots'].size().sort_index()
     gpr_counts = plots.groupby('zone')['gpr'].apply(lambda x: x.notna().sum()).sort_index()
-    gpr_percent = round((gpr_counts / plot_counts) * 100, 2).rename('gpr_%')
+    gpr_percent = round((gpr_counts / plot_counts) * 100, 2).rename('gpr_percent')
 
     gfas = get_gfas(endpoint)
     gfas_counts = gfas.groupby("plots")["gfa_value"].size().rename('gfa_counts')
@@ -109,36 +117,44 @@ def get_gfa_overview(endpoint, plots, non_gfa_plots, out_dir):
     gfas = gfas.groupby("plots")["gfa_value"].min()
     plots = plots.merge(gfas, on='plots', how='outer')
     gfa_counts = plots.groupby('zone')['gfa_value'].apply(lambda x: x.notna().sum()).sort_index().rename('gfa')
-    gfa_percent = round((gfa_counts / plot_counts) * 100, 2).rename('gfa_%')
+    gfa_percent = round((gfa_counts / plot_counts) * 100, 2).rename('gfa_percent')
 
-    excl_plots = plots.loc[plots['plots'].isin(non_gfa_plots)].groupby('zone')['plots'].count().astype(int).fillna(
-        0).sort_index().rename('excl.')
-    multiple_gfas = plots.loc[plots['gfa_counts'] > 1].groupby('zone')['plots'].count().sort_index().rename('> 1 GFAs')
+    gpr_gfa_delta = gfa_percent - gpr_percent
+    gpr_gfa_delta = gpr_gfa_delta.rename('delta')
+
+    exc_plots = plots.loc[plots['plots'].isin(non_gfa_plots)]
+    exc_plots = exc_plots.groupby('zone')['plots'].count().astype(int).fillna(0).sort_index().rename('excluded')
+
+    multiple_gfas = plots.loc[plots['gfa_counts'] > 1].groupby('zone')['plots'].count().sort_index().rename('GFAs >1')
+
     gfas_per_plot = round(plots.groupby('zone')['gfa_counts'].mean().sort_index(), 2).rename('GFAs / plot')
 
     df = pd.concat(
-        [plot_counts, gpr_counts, gpr_percent, gfa_counts, gfa_percent, excl_plots, multiple_gfas, gfas_per_plot],
-        axis=1, join='outer'
+        [plot_counts, gpr_counts, gpr_percent, gfa_counts, gfa_percent, gpr_gfa_delta, exc_plots, multiple_gfas,
+         gfas_per_plot], axis=1, join='outer'
     )
 
     # Prepare the summary row, ignoring zeros in mean calculations
     summary_row = {
         'plots': plot_counts.sum(),
         'gpr': gpr_counts.sum(),
-        'gpr_%': round((gpr_counts.sum() / plot_counts.sum() * 100), 2),
+        'gpr_percent': round((gpr_counts.sum() / plot_counts.sum() * 100), 2),
         'gfa': gfa_counts.sum(),
-        'gfa_%': round((gfa_counts.sum() / plot_counts.sum() * 100), 2),
-        'excl.': excl_plots.sum(),
-        '> 1 GFAs': multiple_gfas.sum(),
+        'gfa_percent': round((gfa_counts.sum() / plot_counts.sum() * 100), 2),
+        'delta': (round((gfa_counts.sum() / plot_counts.sum() * 100), 2)
+                  - round((gpr_counts.sum() / plot_counts.sum() * 100), 2)),
+        'excluded': exc_plots.sum(),
+        'GFAs >1': multiple_gfas.sum(),
         'GFAs / plot': gfas_per_plot[gfas_per_plot != 0].mean()
     }
 
     df.loc['Summary'] = summary_row
+    df = df.fillna(0)
 
     if out_dir:
         output_file = os.path.join(out_dir, 'output_table_4.csv')
         df.to_csv(output_file, index=True)
-        print(f"DataFrame written to {out_dir}")
+        logger.info(f"Data for Table 4 saved to {out_dir}")
 
     return df
 
@@ -152,12 +168,14 @@ def plot_iop(plots, reg_links, out_dir):
     :param out_dir: Directory to save the output image.
     """
 
+    # Aggregate regulation counts per plot
     reg_count = reg_links.groupby('plots').size().reset_index(name='iop')
     plots = plots.merge(reg_count, on='plots')
 
     # Define boundaries and colormap
-    boundaries = np.arange(0, max(reg_links.groupby('plots')['reg'].nunique()), 1)
-    custom_cmap = LinearSegmentedColormap.from_list('CustomGradient', ["#aebfb6", "#902925"])
+    max_reg = reg_links.groupby('plots')['reg'].nunique().max()
+    boundaries = np.arange(0, max_reg + 1, 1)  # Ensure the max value is included
+    custom_cmap = LinearSegmentedColormap.from_list('CustomGradient', ["#8FC1B5", "#902925"])
     norm = BoundaryNorm(boundaries, custom_cmap.N)
 
     # Create the figure and axes
@@ -165,17 +183,21 @@ def plot_iop(plots, reg_links, out_dir):
     plots.plot(column='iop', cmap=custom_cmap, norm=norm, ax=ax, alpha=0.9)
     ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron, alpha=0.5)
 
+    # Customize tick parameters (currently ticks are disabled)
     ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-    cbar_ax = fig.add_axes([ax.get_position().x0, ax.get_position().y0 - 0.03, ax.get_position().width, 0.02])
+
+    # Create and customize the colorbar
+    cbar_ax = fig.add_axes([ax.get_position().x0, ax.get_position().y0 - 0.07, ax.get_position().width, 0.02])
     cbar = ColorbarBase(cbar_ax, cmap=custom_cmap, norm=norm, boundaries=boundaries, orientation='horizontal')
-    cbar.set_label('Number of linked regulations to the plot')
+    cbar.set_label('Number of Linked Regulations per Plot', fontsize=8)
+    cbar.ax.tick_params(labelsize=8)
 
     # Save the figure
     output_path = os.path.join(out_dir, "output_figure_5.png")
     plt.savefig(output_path, bbox_inches='tight', dpi=150)
     plt.close(fig)
 
-    print(f"Figure saved to {output_path}")
+    logger.info(f"Figure 5 saved to {out_dir}")
 
 
 def plot_gfa_deltas(endpoint, plots, non_gfa_plots, out_dir):
@@ -195,7 +217,7 @@ def plot_gfa_deltas(endpoint, plots, non_gfa_plots, out_dir):
     gfa_plots['gpr_gfa'] = gfa_plots['gpr'] * gfa_plots['plot_area']
     gfas = get_gfas(endpoint)
     gfas = gfas.groupby("plots")["gfa_value"].min()
-    gfa_plots = gfa_plots.merge(gfas, on='plots', how='inner')
+    gfa_plots = gfa_plots.merge(gfas, on='plots', how='outer')
 
     gfa_plots['gpr_gfa_delta'] = ((gfa_plots['gfa_value'] - gfa_plots['gpr_gfa']) / gfa_plots['gpr_gfa']) * 100
 
@@ -203,21 +225,107 @@ def plot_gfa_deltas(endpoint, plots, non_gfa_plots, out_dir):
     delta_max = gfa_plots['gpr_gfa_delta'].max()
     norm = plt.Normalize(vmin=delta_min, vmax=delta_max)
 
-    custom_cmap = LinearSegmentedColormap.from_list('CustomGradient', ["#902925", "#aebfb6"])
-    fig, ax = plt.subplots(figsize=(8, 8), dpi=150)
+    custom_cmap = LinearSegmentedColormap.from_list('CustomGradient', ["#902925", "#8FC1B5"])
+    fig, ax = plt.subplots(figsize=(6, 6), dpi=150)
     gfa_plots.plot(column='gpr_gfa_delta', cmap=custom_cmap, norm=norm, ax=ax, alpha=0.9)
     ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
     ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron, alpha=0.5)
-    cbar_ax = fig.add_axes([ax.get_position().x0, ax.get_position().y0 - 0.03, ax.get_position().width, 0.02])
+    cbar_ax = fig.add_axes([ax.get_position().x0, ax.get_position().y0 - 0.07, ax.get_position().width, 0.02])
 
     sm = plt.cm.ScalarMappable(norm=norm, cmap=custom_cmap)
     sm.set_array([])
     cbar = plt.colorbar(sm, cax=cbar_ax, orientation='horizontal')
-    cbar.set_label("Relative GFA Δ")
+    cbar.set_label("Relative GFA Δ", fontsize=8)
+    cbar.ax.tick_params(labelsize=8)
 
     # Save the figure
     output_path = os.path.join(out_dir, "output_figure_6.png")
     plt.savefig(output_path, bbox_inches='tight', dpi=150)
-
     plt.close(fig)
 
+    logger.info(f"Figure 6 saved to {out_dir}")
+
+
+def plot_scenario_difference(endpoint, scenario_endpoint, plots, reg_links, out_dir):
+    """
+        Plots the difference in GFA values between the base scenario and an alternative scenario
+        related to Height Control Plans (HCP).
+
+        :param endpoint: The base endpoint URL.
+        :param scenario_endpoint: The scenario-specific endpoint URL.
+        :param plots: GeoDataFrame containing plot data.
+        :param reg_links: DataFrame with regulation and plot links.
+        :param out_dir: Directory to save the output plot.
+        """
+    hcp_plots = plots[plots['plots'].isin(reg_links[reg_links['reg_type'] == 'HeightControlPlan']['plots'])].copy()
+    all_hcp_plots = hcp_plots.copy()
+
+    # Retrieve base GFA values
+    gfas = get_gfas(endpoint)
+    gfas = gfas.groupby("plots")["gfa_value"].min()
+    hcp_plots = hcp_plots.merge(gfas, on='plots', how='left')
+
+    # Retrieve scenario GFA values
+    gfas_scenario = get_gfas(scenario_endpoint)
+    gfas_scenario = gfas_scenario.groupby("plots")["gfa_value"].min().rename('gfa_value_scenario')
+    hcp_plots = hcp_plots.merge(gfas_scenario, on='plots', how='left')
+
+    hcp_plots['scenario_diff'] = hcp_plots['gfa_value_scenario'] - hcp_plots['gfa_value']
+    hcp_plots = hcp_plots[hcp_plots['scenario_diff'] > 10]
+    hcp_base = gpd.GeoDataFrame(geometry=[hcp_plots.buffer(200).unary_union], crs=hcp_plots.crs)
+
+    summary = hcp_plots['scenario_diff'].describe()
+    bins = [0, summary['25%'], summary['50%'], summary['75%'], float('inf')]
+    bins = [round(b, 2) for b in bins]
+
+    dashed_line = Line2D([0], [0], color="#902925", linestyle="--", linewidth=0.5, label="Affected plot location")
+    colors = ["#8FC1B5", "#589A8D", "#007566", "#265C4B"]
+    labels = [f"0-{bins[1]:.2f}", f"{bins[1]:.2f}-{bins[2]:.2f}", f"{bins[2]:.2f}-{bins[3]:.2f}", f">{bins[3]:.2f}"]
+    cmap = ListedColormap(colors)
+    x_min, x_max = hcp_plots.total_bounds[0] + 4000, hcp_plots.total_bounds[2] - 7000
+    y_min, y_max = hcp_plots.total_bounds[1] - 800, hcp_plots.total_bounds[3] - 9000
+
+    fig, ax = plt.subplots(figsize=(6, 6), dpi=150)
+    hcp_base.plot(ax=ax, lw=0.5, color='None', ls='--', edgecolor="#902925", zorder=2)
+    all_hcp_plots.plot(ax=ax, color='grey', alpha=0.3, zorder=1)
+    hcp_plots.plot(column='scenario_diff', cmap=cmap, legend=False, ax=ax, scheme="userdefined",
+                   classification_kwds={"bins": bins}, zorder=3)
+    ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+    ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron, alpha=0.3)
+    patches = [mpatches.Patch(color=colors[i], label=labels[i]) for i in range(len(labels))]
+    patches.append(mpatches.Patch(color='w', label=''))
+    patches.append(mpatches.Patch(color='lightgrey', label='Height Control Plans'))
+    patches.append(dashed_line)
+
+    ax.legend(handles=patches, title="GFA Gain for affected plots", loc='upper right', ncol=1, frameon=True,
+              framealpha=0.9, fontsize=10)
+
+    # Set axis limits for zoom
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    plt.tight_layout()
+
+    output_path = os.path.join(out_dir, "output_figure_7.png")
+    plt.savefig(output_path, bbox_inches='tight', dpi=150)
+    plt.close(fig)
+
+    metrics = {
+        "Metric": [
+            "Total GFA Gain (sqm)",
+            "Number of Affected Plots",
+            "Average GFA Gain Per Plot (sqm)",
+            "Average Plot Size (sqm)"
+        ],
+        "Value": [
+            hcp_plots['scenario_diff'].sum(),
+            len(hcp_plots),
+            round(hcp_plots['scenario_diff'].sum() / len(hcp_plots), 2),
+            round(hcp_plots.area.sum() / len(hcp_plots), 2)
+        ]
+    }
+    metrics_df = pd.DataFrame(metrics)
+
+    output_file = os.path.join(out_dir, 'output_figure_7.csv')
+    metrics_df.to_csv(output_file, index=True)
+
+    logger.info(f"Figure 7 saved to {output_path}")
